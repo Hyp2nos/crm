e9df2800c7788f6df39c317ca630c8bf48c00ae6
ANDRIAMIHARISOA Mamy <andry279@gmail.com>
2024-08-22 17:13:59 +0300

    fix user name

--- src/main/java/com/crm/crm/CrmApplication.java
+++ src/main/java/com/crm/crm/CrmApplication.java
@@ -18,7 +18,7 @@ public class CrmApplication {
     @Bean
     CommandLineRunner commandLineRunner(final CustomerRepository customerRepository) {
         return args -> {
-            customerRepository.save(new Customer(1L, "John", "Doe", "john.doe@example.com", "1234567891"));
+            customerRepository.save(new Customer(1L, "John Louis", "Doe", "john.doe@example.com", "1234567891"));
             customerRepository.save(new Customer(2L, "Kale", "Doe", "john.doe@example.com", "1234567892"));
             customerRepository.save(new Customer(3L, "Tom", "Doe", "john.doe@example.com", "1234567893"));
         };
